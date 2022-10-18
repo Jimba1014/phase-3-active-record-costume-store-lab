91e8943d0a7cbe095a7ed7983b93a45194fc36cf
Jim <jimba.ed@outlook.com>
2022-10-18 12:41:28 -0400

    commit tree 0ceca33e71084425acf376a92a5f1e29ad9273cd

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,17 @@
-# Create your CostumeStore class here
+class CostumeStore <ActiveRecord::Base
+    def change 
+        create_table :costume_store do |t|
+          t.string :name
+          t.string :location
+          t.integer :costume_inventory
+          t.integer :num_of_employees
+          t.boolean :is_in_bussiness
+          t.datetime :opening_time
+          t.date_time :closing_time
+        
+          t.timestamps
+        end
+        
+    end
+
+end
